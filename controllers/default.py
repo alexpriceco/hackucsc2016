984c76c16c49ba5c87bd1223b082d42e8549fab1
pmercado23 <pmercado@ucsc.edu>
2016-01-30 09:48:01 -0800

    working on adding messaging

--- controllers/default.py
+++ controllers/default.py
@@ -63,7 +63,7 @@ def test():
     return dict(experiences_list=experiences_list, form=form)
 
 
-def chat():
+def chat1():
     form = SQLFORM(db.experiences)
     experiences_list = db(db.experiences).select()
 
@@ -76,3 +76,59 @@ def reset():
 
     redirect(URL('default', 'index'))
 
+def people():
+    db.people.name.lable = "Name"
+    q = (db.poeple.id != auth.user_id)
+    links = [dict(header='Click to chat',
+                  body = lambda r:A(I(_class='fa fa-comments'), 'Chat', _class='btn btn-primary btn-lg outline',
+                                    _href=URL('default', 'chat', args=[r.user_id])))]
+    grid = SQLFORM.grid(q,
+                        links = links,
+                        editable=False,
+                        details=False,
+                        csv=False)
+    return dict(grid=grid)
+
+def chat():
+    """This page enables you to chat with another person."""
+    back_button = A(' Back', _class='btn btn-primary btn-lg outline',
+                    _href=URL('default', 'people', user_signature=True))
+    # Let us read the record telling us who is the other person.
+    other = db(db.people.user_id == request.args(0)).select().first()
+    logger.info("I am %r, chatting with %r" % (auth.user_id, other))
+    if other is None:
+        # Back to square 0.
+        return redirect(URL('default', 'index'))
+    # Pair of people involved.
+    two_people = [auth.user_id, other.id]
+    # We want them in order, so that all messages will be stored under the same pairs of ids.
+    two_people.sort()
+    # This query selects all messages between the two people.
+    q = ((db.messages.user0 == two_people[0]) & (db.messages.user1 == two_people[1]))
+    # This is the list of messages.
+    db.messages.sender.represent = lambda v, r: 'You' if v == auth.user_id else other.name
+    grid = SQLFORM.grid(q,
+                        fields=[db.messages.msg_time, db.messages.sender, db.messages.msg_id],
+                        details=False,
+                        create=False,
+                        orderby=~db.messages.msg_time,
+                        csv=False,
+                        sortable=False,
+                        editable=False,
+                        deletable=False,
+                        searchable=False,
+                        user_signature=False)
+
+    # This is a form for adding one more message.
+
+    db.messages.sender.readable = db.messages.sender.writable = False
+    db.messages.msg_time.readable = db.messages.msg_time.writable = False
+    form = SQLFORM(db.messages)
+    form.vars.user0 = two_people[0]
+    form.vars.user1 = two_people[1]
+    if form.process(onvalidation=store_message).accepted:
+        session.flash = "Message sent!"
+        redirect(URL('default', 'chat', args=[other.user_id]))
+
+    title = "Chat with %s" % other.name
+    return dict(title=title, grid=grid, form=form, back_button=back_button)
