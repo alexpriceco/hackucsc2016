1fe5e1ccd8a2e43bf1dfd2a85c5f57584f0d0ad3
pmercado23 <pmercado@ucsc.edu>
2016-01-29 20:33:20 -0800

    building tables to store everything

--- models/tables.py
+++ models/tables.py
@@ -4,8 +4,32 @@ db.define_table('users',
                 Field('user_id', db.auth_user, default=auth.user_id),
                 Field('preferred_named', db.auth_user, default=auth.user_id),
                 Field('photo_id', db.auth_user, default=auth.user_id),
-                Field('karma_points', db.auth_user, default=0)
+                Field('karma_points', db.auth_user, default=0),
+                Field('lessons_competed', db.auth_user, default=0)
 )
+
+db.users.id.readable = db.users.id.writable = False
+db.users.user_id.readable = db.users.user_id.writable = False
+
+
 db.define_table('points',
                 )
+
+db.define_table('experiences',
+                Field('user_id', 'reference users' ),
+                Field('kind', label='type', requires=IS_IN_SET(['Discrimination', 'Workplace_Issue', 'Wage', 'Safety',
+                                                  'Health', 'Protection', 'Wage/hours/scheduling' ]), ),
+
+                Field('story', label='Your Story', type='string'),
+                )
+
+db.experiences.id.readable = db.experiences.id.writable = False
+db.experiences.user_id.readable = db.experiences.user_id.writable = False
+
+db.define_table('responses',
+                Field('q1', ), ##this is where we will put answers to the questions. for now there is nothing
+                ## this is just a place holder
+                Field('qn', ),
+
+)
 
