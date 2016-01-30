bb06ce0d717068e4f55420fab49fb2e9d4cb3e7d
Zach <Aftermath1991@gmail.com>
2016-01-30 13:00:02 -0800

    merging process

--- controllers/default.py
+++ controllers/default.py
@@ -9,15 +9,16 @@
 #########################################################################
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
 
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    db.people.name.label = "What's your name?"
+
+    row = db(db.people.user_id == auth.user_id).select().first()
+    db.people.user_id.readable = db.people.user_id.writable = False
+    form = SQLFORM(db.people, record=row)
+    if form.process().accepted:
+        session.flash = "Welcome, %s!" % form.vars.name
+        redirect(URL('default', 'people'))
+    return dict(form=form)
 
 
 def user():
@@ -57,4 +58,76 @@ def call():
     """
     return service()
 
+def test():
+    form = SQLFORM(db.experiences)
+    experiences_list = db(db.experiences).select()
+    return dict(experiences_list=experiences_list, form=form)
+
+
+def reset():
+    db(db.experiences.id > 0).delete()
+    db(db.users.id > 0).delete()
+    db(db.user_responses.id > 0).delete()
+
+    redirect(URL('default', 'index'))
+
+def people():
+    db.people.name.lable = "Name"
+    q = (db.people.id != auth.user_id)
+    links = [dict(header='Click to chat',
+                  body = lambda r:A(I(_class='fa fa-comments'), 'Chat', _class='btn btn-primary btn-lg outline',
+                                    _href=URL('default', 'chat', args=[r.user_id])))]
+    grid = SQLFORM.grid(q,
+                        links = links,
+                        editable=False,
+                        details=False,
+                        csv=False)
+    return dict(grid=grid)
+
+def chat():
+    """This page enables you to chat with another person."""
+    back_button = A(' Back', _class='btn btn-primary btn-lg outline',
+                    _href=URL('default', 'people', user_signature=True))
+    # Let us read the record telling us who is the other person.
+    other = db(db.people.user_id == request.args(0)).select().first()
+    logger.info("I am %r, chatting with %r" % (auth.user_id, other))
+    if other is None:
+        # Back to square 0.
+        return redirect(URL('default', 'index'))
+    # Pair of people involved.
+    two_people = [auth.user_id, other.id]
+    # We want them in order, so that all messages will be stored under the same pairs of ids.
+    two_people.sort()
+    # This query selects all messages between the two people.
+    q = ((db.messages.user0 == two_people[0]) & (db.messages.user1 == two_people[1]))
+    # This is the list of messages.
+    db.messages.sender.represent = lambda v, r: 'You' if v == auth.user_id else other.name
+    grid = SQLFORM.grid(q,
+                        fields=[db.messages.msg_time, db.messages.sender, db.messages.msg_id],
+                        details=False,
+                        create=False,
+                        orderby=~db.messages.msg_time,
+                        csv=False,
+                        sortable=False,
+                        editable=False,
+                        deletable=False,
+                        searchable=False,
+                        user_signature=False)
+
+    # This is a form for adding one more message.
+
+    db.messages.sender.readable = db.messages.sender.writable = False
+    db.messages.msg_time.readable = db.messages.msg_time.writable = False
+    form = SQLFORM(db.messages)
+    form.vars.user0 = two_people[0]
+    form.vars.user1 = two_people[1]
+    if form.process(onvalidation=store_message).accepted:
+        session.flash = "Message sent!"
+        redirect(URL('default', 'chat', args=[other.user_id]))
+
+    title = "Chat with %s" % other.name
+    return dict(title=title, grid=grid, form=form, back_button=back_button)
+
+def store_message(form):
+    form.vars.msg_id = str(db2.textblob.insert(mytext = form.vars.msg_id))
 
--- models/tables.py
+++ models/tables.py
@@ -1,11 +1,86 @@
 ## This file defines the tables to be used
 
+from datetime import datetime
+
+# This is a table for all users.
+db.define_table('people',
+    Field('user_id', db.auth_user, default=auth.user_id),
+    Field('name', required=True),
+    Field('description', 'text'),
+    )
+
+db.people.id.readable = False
+db.people.user_id.readable = False
+db.people.description.represent = lambda v, r: DIV(v, _class="msg_content")
+
+
+# Here is a table for messages.
+db.define_table('messages',
+    Field('user0', db.auth_user),
+    Field('user1', db.auth_user),
+    Field('sender',  db.auth_user, default=auth.user_id),
+    Field('msg_time', 'datetime', default=datetime.utcnow()),
+    Field('msg_id', 'text')) # Stored as a string
+
+db.messages.user0.readable = db.messages.user0.writable = False
+db.messages.user1.readable = db.messages.user1.writable = False
+db.messages.msg_time.label = "Time"
+db.messages.msg_id.label = "Message"
+db.messages.msg_id.represent = lambda v, r: get_text(v)
+
+def get_text(v):
+    r = db2.textblob(v)
+    return '' if r is None else r.mytext
+
+# Table for big chunks of text.
+db2.define_table('textblob',
+    Field('mytext', 'text'),
+    )
+
+
+
 db.define_table('users',
                 Field('user_id', db.auth_user, default=auth.user_id),
                 Field('preferred_named', db.auth_user, default=auth.user_id),
                 Field('photo_id', db.auth_user, default=auth.user_id),
-                Field('karma_points', db.auth_user, default=0)
+                Field('karma_points', db.auth_user, default=0),
+                Field('admin_prefix', db.auth_user, default=0),
+                Field('lessons_completed', db.auth_user, default=0)
 )
+
+db.users.id.readable = db.users.id.writable = False
+db.users.user_id.readable = db.users.user_id.writable = False
+
+
 db.define_table('points',
                 )
 
+db.define_table('experiences',
+                Field('user_id', 'reference users' ),
+                Field('kind', label='Type of incident', requires=IS_IN_SET(['Discrimination', 'Workplace_Issue', 'Wage', 'Safety',
+                                                  'Health', 'Protection', 'Wage/hours/scheduling' ]), ),
+
+                Field('story', label='Your Story', type='string'),
+                )
+
+db.experiences.id.readable = db.experiences.id.writable = False
+db.experiences.user_id.readable = db.experiences.user_id.writable = False
+
+db.define_table('user_responses',
+                Field('user_id', 'reference users'),
+                Field('q1', ), ##this is where we will put answers to the questions. for now there is nothing
+                ## this is just a place holder
+                Field('qn', ),
+
+)
+
+db.user_responses.id.readable = db.user_responses.id.writable = False
+
+
+##---------------------
+## Creating a form for users to post and read other peoples stories, or create a disccsutuon betwween
+##--------------------
+
+db.define_table('story',
+                Field('user_id', 'reference users'),
+                )
