200e891144b324f5548fc2782fdd87519a54e5a8
Zach <Aftermath1991@gmail.com>
2016-01-29 19:26:18 -0800

    started work on database definitions

--- models/tables.py
+++ models/tables.py
@@ -1,9 +1,11 @@
 ## This file defines the tables to be used
 
 db.define_table('users',
-                Field('user_id', db.auth_user, default=auth.user_id ),
-
-
-
-
+                Field('user_id', db.auth_user, default=auth.user_id),
+                Field('preferred_named', db.auth_user, default=auth.user_id),
+                Field('photo_id', db.auth_user, default=auth.user_id),
+                Field('karma_points', db.auth_user, default=0)
 )
+db.define_table('points',
+                )
+
